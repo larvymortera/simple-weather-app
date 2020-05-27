00b2f9cfa039cd1302ff1d440a7af36c655bddb6
Larvy Mortera <larvy.mortera@codeandtheory.com>
2020-05-28 01:14:08 +0800

    request weather data function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// openweathermap.org api.
+const appId = '695d87a5accb140db2d91c0b5df5d8c7';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
+// Fetch weather data from openweathermap.org.
+const fetchWeatherData = (location) => {
+  return fetch(`${apiUrl}?q=${location}&appId=${appId}&units=metric`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return {
+        name: data.name,
+        temp: data.main.temp,
+        timezone: data.timezone,
+        weather: data.weather[0] ?? {},
+      };
+    });
+};
