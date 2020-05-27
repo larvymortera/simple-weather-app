176b6202a349fdd1bb2df33cc37e62749b143529
Larvy Mortera <larvy.mortera@codeandtheory.com>
2020-05-28 04:20:37 +0800

    log current date and time

--- script.js
+++ script.js
@@ -22,3 +22,13 @@ const fetchWeatherData = (location) => {
       };
     });
 };
+
+// Get date and time.
+const getCurrentDateTime = (offset) => {
+  const localDate = new Date();
+  const totalOffset = (localDate.getTimezoneOffset() * 60 * 1000) + (offset * 1000);
+  const date = new Date(localDate.getTime() + totalOffset);
+  const hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
+  const minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
+  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} ${hours}:${minutes}`;
+};
